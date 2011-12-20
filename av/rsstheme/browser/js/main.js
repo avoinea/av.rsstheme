626bca550e438df020a3d432aa414cd512ade076
Alin Voinea <alin@serenity>
2011-12-20 15:55:14 +0200

    - Updated theme and our custom faceted listing.

---
+++ av/rsstheme/browser/js/main.js
@@ -0,0 +1,98 @@
+jQuery.fn.rodate = function(options){
+  var settings = {};
+
+  var roformat = function(date){
+    var now = jQuery('#current-time');
+    if(now.length){
+      now = new Date(now.text());
+    }else{
+      now = new Date();
+    }
+    var past = new Date(date.text());
+    var delta = (now-past)/1000;
+    if(delta < 0){
+      date.text('acum cateva secunde');
+      return;
+    }
+    // Minutes
+    if(delta < 120){
+      date.text('acum un minut');
+      return;
+    }
+    if(delta < 3300){
+      var min = parseInt(delta/60, 10);
+      date.text('acum ' + min + ' minute');
+      return;
+    }
+    // Hours
+    if(delta < 7200){
+      date.text('acum o ora');
+      return;
+    }
+    if(delta < 72000){
+      var hours = parseInt(delta / 3600, 10);
+      date.text('acum ' + hours + ' ore');
+      return;
+    }
+    // Days
+    delta = delta / 3600;
+    if(delta < 48){
+      date.text('ieri');
+      return;
+    }
+    if(delta < 160){
+      var days = parseInt(delta / 24, 10);
+      date.text('acum ' + days + ' zile');
+      return;
+    }
+    // Weeks
+    if(delta < 336){
+      date.text('saptamana trecuta');
+      return;
+    }
+    if(delta < 720){
+      var weeks = parseInt(delta/24/7, 10);
+      date.text('acum ' + weeks + ' saptamani');
+      return;
+    }
+    // Months
+    delta = delta / 24;
+    if(delta < 60){
+      date.text('acum o luna');
+      return;
+    }
+    if(delta < 360){
+      var months = parseInt(delta/30, 10);
+      date.text('acum ' + months + ' luni');
+      return;
+    }
+    // Years
+    if(delta < 720){
+      date.text('acum un an');
+      return;
+    }
+    if(delta>720){
+      var years = parseInt(delta/360, 10);
+      date.text('acum ' + years + ' ani');
+    }
+  };
+
+  return this.each(function(){
+    if(options){
+      jQuery.extend(settings, options);
+    }
+
+    var self = jQuery(this);
+    roformat(self);
+  });
+};
+
+jQuery(document).ready(function(){
+  jQuery(Faceted.Events).bind(Faceted.Events.AJAX_QUERY_SUCCESS, function(){
+    var dates = jQuery('.ro-date');
+    if(!dates.length){
+      return;
+    }
+    dates.rodate();
+  });
+});
